c25ceabae7871497fb0e5492ff0193285dead5fb
Erikas Neverdauskas <erikasnever@hotmail.com>
2013-11-06 20:46:26 +0200

    added 3 laboras project

---
+++ 3_Laboras/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_3_Laboras.Startup))]
+namespace _3_Laboras
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
